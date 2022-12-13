dd2cf05424a68df5499ea71dfbd8153cbf113736
Yavor Radulov <raduloov@gmail.com>
2022-12-13 10:38:38 +0200

    add viewmodel, app module, data.json and readJSON util

---
+++ app/src/main/java/com/example/cathub/di/AppModule.kt
@@ -0,0 +1,4 @@
+package com.example.cathub.di
+
+class AppModule {
+}
---
+++ app/src/main/java/com/example/cathub/domain/model/Cat.kt
@@ -0,0 +1,7 @@
+package com.example.cathub.model
+
+data class Cat(
+    val name: String,
+    val breed: String,
+    val color: String
+    )
